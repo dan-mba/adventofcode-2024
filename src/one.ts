a6a9d3ddf5d27f7a3600d339f243d4aac982cf2e
Dan <35927536+dan-mba@users.noreply.github.com>
2024-12-02 13:00:29 -0500

    solved day one

--- src/one.ts
+++ src/one.ts
@@ -3,11 +3,30 @@ import { join } from "node:path";
 import { cwd } from "node:process";
 
 let fh;
+let first:number[] = [];
+let second:number[] = [];
+let sum = 0;
 
 try {
   fh = await open(join(cwd(),"input/one.txt"), "r");
   const data = await fh.readFile({encoding: "utf-8"});
-  console.log(data.length)
+  const lines = data.split("\n");
+  lines.forEach(line => {
+    const vals = line.split(/\s+/);
+    if(vals.length !== 2) {
+      return;
+    }
+    first.push(Number(vals[0]));
+    second.push(Number(vals[1]));
+  })
+  
+  first.sort((a,b) => a-b);
+  second.sort((a,b) => a-b);
+  for (let i=0; i<first.length; i++) {
+    sum += Math.abs((first[i] || 0) - (second[i] || 0));
+  }
+
+  console.log(sum);
 } finally {
   await fh?.close()
 }
