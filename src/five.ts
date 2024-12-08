372b30651eb22e13d3053505fb23e3aa1b004162
Dan <35927536+dan-mba@users.noreply.github.com>
2024-12-08 15:08:13 -0500

    solve day 5 part 2

--- src/five.ts
+++ src/five.ts
@@ -3,6 +3,7 @@ import { join } from "node:path";
 import { cwd } from "node:process";
 
 let fh;
+const fails:string[][] = [];
 
 function pageRules(page: string, rules:string[][]):string[] {
   const match = rules.filter(r => r[0] === page);
@@ -21,6 +22,28 @@ function checkRules(pages:string[], rules:string[][]):number {
   return Number(pages[Math.floor(pages.length/2)]);
 }
 
+function fixRules(pages:string[], rules:string[][]):number {
+  let list = pages.slice(0);
+
+  for(let i=1; i<list.length; i++) {
+    const page = list[i];
+    const r = pageRules(page ?? '', rules);
+    if (r.length === 0) continue;
+
+    const indexes = r.map(x => list.indexOf(x)).filter(x => x<i);
+    if (indexes.length === 0) continue;
+
+    indexes.sort((a, b) => a-b);
+    const swap = indexes[indexes.length-1];
+    if (swap === -1) continue; 
+
+    const temp = list.toSpliced(i, 1);
+    list = [...temp.slice(0,swap), page ?? '', ...temp.slice(swap) ];
+    i=1;
+  }
+  return Number(list[Math.floor(list.length/2)]);
+}
+
 try {
   fh = await open(join(cwd(),"input/five.txt"), "r");
   const data = await fh.readFile({encoding: "utf-8"});
@@ -28,8 +51,14 @@ try {
   const rules = pages[0]?.split("\n").map(x => x.split("|")) ?? [[]];
   const prints = pages[1]?.split("\n").slice(0,-1).map(x => x.split(",")) ?? [[]];
 
-  const mid = prints.reduce((sum, p) => sum + checkRules(p, rules), 0);
+  const mid = prints.reduce((sum, p) => {
+    const m = checkRules(p, rules);
+    if (m === 0 ) fails.push(p);
+    return sum + m;
+  }, 0);
   console.log(mid);
+  const midFixed = fails.reduce((sum, p) => sum + fixRules(p, rules), 0);
+  console.log(midFixed)
 } finally {
   await fh?.close()
 }
