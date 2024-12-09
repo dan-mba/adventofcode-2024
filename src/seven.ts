8387dacb70f64b968b1f745b419e191faa29b70d
Dan <35927536+dan-mba@users.noreply.github.com>
2024-12-09 08:41:05 -0500

    day 7 part 1

---
+++ src/seven.ts
@@ -0,0 +1,37 @@
+import { open } from "node:fs/promises";
+import { join } from "node:path";
+import { cwd } from "node:process";
+import { addSyntheticLeadingComment } from "typescript";
+
+let fh;
+let sum = 0;
+
+function operators (a:number, b:number):number[] {
+  return [a+b, a*b];
+}
+
+try {
+  fh = await open(join(cwd(),"input/seven.practice.txt"), "r");
+  const data = await fh.readFile({encoding: "utf-8"});
+  const lines = data.split("\n");
+  const equations = lines.map(l => l.split(": "));
+
+  equations.forEach(eq => {
+    if (eq.length !== 2) return;
+    const ans = Number(eq[0]);
+    const operands = (eq[1] ?? "").split(" ").map(x => Number(x));
+    let tries = operators(operands[0] ?? 0, operands[1] ?? 0);
+    const nums = operands.slice(2);
+    while (nums.length > 0) {
+      const n = nums.shift();
+      tries = tries.flatMap(x => operators(x, n ?? 0));
+    }
+    if (tries.includes(ans)) {
+      sum += ans;
+    }
+  })
+
+  console.log(sum);
+} finally {
+  await fh?.close()
+}
