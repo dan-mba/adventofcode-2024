d5871214d5c21f957cef8928d76e166185807b66
Dan <35927536+dan-mba@users.noreply.github.com>
2024-12-17 11:34:38 -0500

    day 17 part 1 solved + part 2 wip

---
+++ src/seventeen.ts
@@ -0,0 +1,148 @@
+import { SQRT1_2 } from "mathjs";
+import { open } from "node:fs/promises";
+import { join } from "node:path";
+import { cwd } from "node:process";
+import type { isBreakOrContinueStatement } from "typescript";
+
+let fh;
+let registersObject = {
+  a: 0,
+  b: 0,
+  c: 0
+};
+let initialRegisters = structuredClone(registersObject);
+
+let program = [];
+let output:number[] = [];
+let instructionPointer = 0;
+
+function getProgram(sections: string[]): number[] {
+  if (sections.length < 2) return [];
+
+  const registers = sections[0]?.split("\n");
+  if (registers) {
+    registers.forEach(r => {
+      const reg = r.split(":");
+      switch(reg[0]?.slice(-1)) {
+        case "A":
+          registersObject.a = Number(reg[1]);
+          break;
+        case "B":
+          registersObject.b = Number(reg[1]);
+          break;
+        case "C":
+          registersObject.c = Number(reg[1]);
+          break;
+      }
+    })
+    initialRegisters = structuredClone(registersObject)
+  }
+
+  const program = sections[1]?.split(": ")[1];
+  return program?.split(",").map(x => Number(x)) ?? [];
+}
+
+function getCombo(operand:number):number {
+  if(operand < 0 || operand > 6) return 0;
+  switch (operand) {
+    case 4:
+      return registersObject.a;
+    case 5:
+      return registersObject.b;
+    case 6:
+      return registersObject.c;
+  }
+  return operand
+}
+
+function runOp(opcode: number, operand:number):void {
+  switch(opcode) {
+    case 0:
+      const d = Math.pow(2, getCombo(operand));
+      registersObject.a = Math.floor(registersObject.a / d);
+      return;
+    case 1:
+      registersObject.b = registersObject.b ^ operand;
+      return;
+    case 2:
+      registersObject.b = getCombo(operand) % 8;
+      return;
+    case 3:
+      if (registersObject.a ===0) return;
+      instructionPointer = operand;
+      return;
+    case 4:
+      registersObject.b = registersObject.b ^ registersObject.c;
+      return;
+    case 5:
+      output.push(getCombo(operand) % 8);
+      return;
+    case 6:
+      const d2 = Math.pow(2, getCombo(operand));
+      registersObject.b = Math.floor(registersObject.a / d2);
+      return;
+    case 7:
+      const d3 = Math.pow(2, getCombo(operand));
+      registersObject.c = Math.floor(registersObject.a / d3);
+      return;
+  }
+}
+
+try {
+  fh = await open(join(cwd(),"input/seventeen.txt"), "r");
+  const data = await fh.readFile({encoding: "utf-8"});
+  const sections = data.split("\n\n");
+
+  program = getProgram(sections);
+
+  while (program.length - instructionPointer >= 2) {
+    const opcode = program[instructionPointer] ?? 8;
+    const operand = program[instructionPointer + 1] ?? 8;
+    instructionPointer += 2;
+    runOp(opcode, operand)
+  }
+
+  console.log(output?.join(","));
+
+  let aValue = 0;
+  let bits = [];
+  let bitNumber = 0
+  const programString = program.join(",");
+  while (true){
+    const currentString = program.slice(-1*(bits.length+1)).join(",");
+    while (true) {
+      registersObject = structuredClone(initialRegisters);
+      registersObject.a = aValue;
+      instructionPointer = 0;
+      output = [];
+
+      while (program.length - instructionPointer >= 2) {
+        const opcode = program[instructionPointer] ?? 8;
+        const operand = program[instructionPointer + 1] ?? 8;
+        instructionPointer += 2;
+        runOp(opcode, operand)
+      }
+
+      const outputString = output?.join(",")
+      if (outputString == currentString) {
+        bits.push(aValue);
+        console.log(aValue.toString(8))
+        break;
+      }
+    
+      aValue += 1;
+
+    }
+    if (currentString == programString) {
+      break;
+    }
+    bitNumber = aValue
+    aValue = bitNumber * 8;
+    console.log(aValue.toString(8))
+    console.log(currentString)
+  }
+
+  console.log(aValue);
+} finally {
+  await fh?.close()
+}
