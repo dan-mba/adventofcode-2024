51252bbf5bd281466001c2dad3a7c70c60cccc29
Dan <35927536+dan-mba@users.noreply.github.com>
2024-12-22 11:25:04 -0500

    day 22 part 1

--- src/twenty-two.ts
+++ src/twenty-two.ts
@@ -1,3 +1,4 @@
+import { setCartesian } from "mathjs";
 import { open } from "node:fs/promises";
 import { join } from "node:path";
 import { cwd } from "node:process";
@@ -12,17 +13,34 @@ function prune(secret:number):number {
   return secret % 16777216;
 }
 
-function calcuateNext(secret:number) {
+function calculateNext(secret:number) {
   let next =  prune(mix(secret, secret*64));
   next = prune(mix(next, Math.floor(next/32)));
-  return prune(mix(next, next*2048))
+  return prune(mix(next, next*2048));
+}
+
+function twoThousandSecret(secret:number): number {
+  let next = secret;
+  for (let i = 0; i< 2000; i++) {
+    next = calculateNext(next);
+  }
+  return next;
 }
 
 try {
-  fh = await open(join(cwd(),"input/twenty-two.practice.txt"), "r");
+  fh = await open(join(cwd(),"input/twenty-two.txt"), "r");
   const data = await fh.readFile({encoding: "utf-8"});
+  const secrets = data.split("\n").slice(0,-1);
+  const secretNums = secrets.map(x => Number(x));
 
-  
+  let sum = 0;
+  for (const secret of secretNums) {
+    const next = twoThousandSecret(secret);
+    console.log(next);
+    sum += next;
+  }
+
+  console.log("sum: ", sum);
 } finally {
   await fh?.close()
 }
